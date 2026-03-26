436d67c62cee45af4d2085b693b18a806f9875fd
sakshi-gambhire <ssgambhire370122@kkwagh.edu.in>
2026-03-26 23:37:56 +0530

    fix CORS issue

--- server.js
+++ server.js
@@ -33,7 +33,11 @@ const app = express();
 
 // Middleware
 
-app.use(cors());
+
+app.use(cors({
+  origin: ["http://localhost:5173"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/bias", biasRoutes);
